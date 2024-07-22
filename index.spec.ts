9b6907cdb2bf10f098bcc30150badf562e1a1e92
Mark Jaspher Ramile <jramile@sorenson.com>
2024-07-22 11:45:05 +0800

    * init commit

---
+++ index.spec.ts
@@ -0,0 +1,44 @@
+import { InternalAxiosRequestConfig } from 'axios';
+import { paramsInterceptor } from './path-to-your-interceptor-file';
+
+describe('paramsInterceptor', () => {
+  it('should set lob to SLOCPI if params or entity is undefined', async () => {
+    const requestConfig: InternalAxiosRequestConfig = {};
+
+    const result = await paramsInterceptor()(requestConfig);
+
+    expect(result.params).toEqual({ lob: 'SLOCPI' });
+  });
+
+  it('should set lob to SLOCPI if entity is not slgfi', async () => {
+    const requestConfig: InternalAxiosRequestConfig = { params: { entity: 'other' } };
+
+    const result = await paramsInterceptor()(requestConfig);
+
+    expect(result.params).toEqual({ entity: 'other', lob: 'SLOCPI' });
+  });
+
+  it('should set lob to SLGFI if entity is slgfi', async () => {
+    const requestConfig: InternalAxiosRequestConfig = { params: { entity: 'slgfi' } };
+
+    const result = await paramsInterceptor()(requestConfig);
+
+    expect(result.params).toEqual({ entity: 'slgfi', lob: 'SLGFI' });
+  });
+
+  it('should set lob to SLGFI if entity is SLGFI (case insensitive)', async () => {
+    const requestConfig: InternalAxiosRequestConfig = { params: { entity: 'SLGFI' } };
+
+    const result = await paramsInterceptor()(requestConfig);
+
+    expect(result.params).toEqual({ entity: 'SLGFI', lob: 'SLGFI' });
+  });
+
+  it('should preserve existing params and add lob', async () => {
+    const requestConfig: InternalAxiosRequestConfig = { params: { entity: 'slgfi', otherParam: 'test' } };
+
+    const result = await paramsInterceptor()(requestConfig);
+
+    expect(result.params).toEqual({ entity: 'slgfi', otherParam: 'test', lob: 'SLGFI' });
+  });
+});
